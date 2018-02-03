fd89a2ad87c899c92b6f723256677c8bcd7b3fd4
r4ch3t <williams.a.hunter99@gmail.com>
2018-02-03 17:14:50 +0000

    Change the prompt

--- greeting.rb
+++ greeting.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 require 'colorize'
-puts "Please enter your name."
+print "Please enter your name: "
 name = gets.chomp.colorize(:blue) 
 puts "Hi, #{name}. I'm Jarvis, the not so autonomous machine."
